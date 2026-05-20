68f04f55a639b873acd545d9fa8edd6c68b9ee04
Luiz-M-Marangon <luizmarangonzz11@gmail.com>
2026-05-20 19:30:57 -0300

    ih lá vamos nóóóóóiiisss

---
+++ src/main/java/gerenciamento/quadra/frameworks/FrameworksApplication.java
@@ -0,0 +1,13 @@
+package gerenciamento.quadra.frameworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gerenciamento/quadra/frameworks/FrameworksApplicationTests.java
@@ -0,0 +1,13 @@
+package gerenciamento.quadra.frameworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
